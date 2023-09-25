7385b53aa201472e0ea2a2c86f26cee1d4b4485d
Idalis Valadez <idalisvaladez22@gmail.com>
2023-09-24 20:11:47 -0500

    sqlite, Player name property, table classmethods, create player, player.db, welcome, start, and intro storyline

--- cli.py
+++ cli.py
@@ -1,55 +1,65 @@
 import argparse
-from game.game_logic import Game
-from game.player import Player 
-from termcolor import cprint
+from game.player import Player
+from game.inventory import Inventory 
 from pyfiglet import Figlet
+from termcolor import cprint
 import time
 
-def welcome():
-    figlet = Figlet(font='isometric2', width= 200)
-    cprint(figlet.renderText('The Escapist!'), 'green')
-    time.sleep(1.0)
-
-    
+from helpers import (
+    welcome,
+    wall_one,
+    storyline,
+    create_player,
+    find_player_by_name
+)
 
 def main():
-    parser = argparse.ArgumentParser(description='The Escapist Game CLI')
-    parser.add_argument('create_player', nargs='?', default=None)
-    args = parser.parse_args()
+    cprint("New player?")
+    choice = input("[y/ N]")
+    if choice == "y":
+        create_player()
+    elif choice == "N":
+        find_player_by_name()
+    # parser = argparse.ArgumentParser(description='The Escapist Game CLI')
+    # parser.add_argument('create_player', nargs='?', default=None)
+    # args = parser.parse_args()
 
-    # Initialize the game
-    game = Game()
+    # # Initialize the game
+    # game = Game()
     
-    player_name = input("Enter the player's name: ")
-    try:
-            # Create a new player
-        player = Player(player_name)
-        game.create_player(player)
-        cprint(f"Player '{player_name}' created successfully.", 'green' , attrs=['bold'])
-        with open('player.txt', 'w') as file:
-            file.write(player_name)
-    except ValueError as e:
-        print(str(e))
+    # player_name = input("Enter the player's name: ")
 
-    
-def wall_one():
-    cprint("After a day of sunbathing in the garden, Sofia needs to go to sleep.", 'yellow', attrs=["bold"])
-    cprint("She walks through all the doors once and closes them behind her.", 'yellow', attrs=["bold"])
-    cprint("Which room is her bedroom?", 'yellow', attrs=["bold"])
-    cprint("              ____   _________   _____________   _____", 'green')
-    cprint("              |   \  |        \   |         | \   F  |", 'green')
-    cprint("              |      |            |    D    |__    __|", 'green')
-    cprint("               /  A  |            |         |   \    |", 'green')
-    cprint("              |       /           |        \         |", 'green')
-    cprint("              |__  \_|            |__    \__|    G   |", 'green')
-    cprint("              |      |      C     |         |        |", 'green')
-    cprint("              |      |            |     E    \       |", 'green')
-    cprint("              |   B  |            |         |___  \__|", 'green')
-    cprint("              |      |             /       \      H  |", 'green')
-    cprint("              |_  \____/__________|_________|________|", 'green')
+    # try:
+    #         # Create a new player
+    #     player = Player(player_name)
+    #     game.create_player(player)
+    #     cprint(f"Player '{player_name}' created successfully.", 'green' , attrs=['bold'])
+    #     with open('player.txt', 'w') as file:
+    #         file.write(player_name)
+    # except ValueError as e:
+    #     print(str(e))
+
+def start():
+    cprint("Ready?")
+    choice = input("[y/ N] > ")
+    if choice == 'y':
+        storyline()
+    elif choice == 'N':
+        figlet = Figlet(font='ogre', width= 100)
+        cprint(figlet.renderText('Game Over!'), 'red', attrs=["bold"])
+
+
+def first_wall():
+    time.sleep(0.5)
+    wall_one()
+    choice = input("> ")
+    if choice == "B":
+        cprint("CORRECT!", "white", "on_green", attrs=['bold'])
+    else:
+        cprint("TRY AGAIN!", 'white', 'on_red', attrs=["bold"])
 
 
 if __name__ == "__main__":
     welcome()
     main()
-    # wall_one()
+    start()
